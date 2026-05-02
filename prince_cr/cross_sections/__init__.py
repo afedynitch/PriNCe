1594196b614364cbe6cfe7d991136dc0511f370f
Anatoli <afedynitch@gmail.com>
2026-05-02 16:00:49 +0800

    Removed orphan files

--- prince_cr/cross_sections/__init__.py
+++
@@ -1,8 +0,0 @@
-from prince_cr.cross_sections.disintegration import (
-    CompositeCrossSection as CompositeCrossSection,
-    TabulatedCrossSection as TabulatedCrossSection,
-)
-from prince_cr.cross_sections.photo_meson import (
-    SophiaSuperposition as SophiaSuperposition,
-    EmpiricalModel as EmpiricalModel,
-)
--- prince_cr/solvers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .propagation import (
-    UHECRPropagationResult as UHECRPropagationResult,
-    UHECRPropagationSolverBDF as UHECRPropagationSolverBDF,
-    UHECRPropagationSolverEULER as UHECRPropagationSolverEULER,
-)
